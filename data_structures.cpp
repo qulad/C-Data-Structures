3350fe369896d9722d0a7a47ec2ea84c947b7971
Ahmet E. Kaya <52331191+qulad@users.noreply.github.com>
2022-11-16 00:12:59 +0300

    Update data_structures.cpp
    Removed BFS and DFS, Transformed rest to template typenames for more type flexibility

--- data_structures.cpp
+++ data_structures.cpp
@@ -1,494 +1,256 @@
-#include <bits/stdc++.h>
 #include <iostream>
 
-using namespace std;
-
 // -------------------- NODE --------------------
-class Node {
-	public:
-		// Constructors
-		Node(); // Empty constructor
-		Node(auto);
-		Node(auto, Node*);
-		Node(Node*); // Copy constructor
-		// Functions
-		int setValue(auto);
-		int setNext(auto);
-		int setPrevious(auto);
-		auto getValue();
-		Node* getNext();
-		Node* getPrevious();
-	private:
-		void *value;
-		Node* next;
-		Node* previous;
+template <typename T> class Node {
+    public:
+        // Constructors
+        Node(); // Empty Constructor
+        Node(T);
+        Node(const Node<T>&); // Copy Constructor
+        // Functions
+        T getValue();
+        Node<T>* getNext();
+        Node<T>* getPrevious();
+        void setValue(T);
+        void setNext(Node<T>*);
+        void setPrevious(Node<T>*);
+    private:
+        T Value;
+        Node<T>* Next;
+        Node<T>* Previous;
 };
-typedef Node* nodePtr;
-
-// ---------- Node Counstructors ----------
-Node::Node() {
-	value = 0;
-	next = NULL;
-	previous = NULL;
+// ---------- NODE CONSTRUCTORS ----------
+template <typename T> Node<T>::Node() {
 };
-Node::Node(auto Value) {
-	value = Value;
-	next = NULL;
-	previous = NULL;
+template <typename T> Node<T>::Node(T value) {
+    Value = value;
 };
-Node::Node(auto Value, Node* Next) {
-	value = Value;
-	next = Next;
-	previous = NULL;
+template <typename T> Node<T>::Node(const Node<T>& copy) {
+    Value = copy->getValue();
+    Next = copy->getNext();
+    Previous = copy->getPrevious();
 };
-Node::Node(Node* node) {
-	value = node->value;
-	next = node->next;
-	previous = NULL;
+// ---------- NODE FUNCTIONS ----------
+template <typename T> T Node<T>::getValue(void) {
+    return Value;
 };
-
-// ---------- Node Functions ----------
-int Node::setValue(auto Value) {
-	try {
-		value = Value;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> Node<T>* Node<T>::getNext(void) {
+    return Next;
 };
-int Node::setNext(auto Value) {
-	try {
-		Node* temp = new Node(Value);
-		next = temp;
-		temp->previous = this;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> Node<T>* Node<T>::getPrevious(void) {
+    return Previous;
 };
-int Node::setPrevious(auto Value) {
-	try {
-		Node* temp = new Node(Value);
-		previous = temp;
-		previous->next = this;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> void Node<T>::setValue(T Value) {
+    this->Value = Value;
 };
-auto Node::getValue() {
-	return value;
+template <typename T> void Node<T>::setNext(Node<T>* Next) {
+    this->Next = Next;
 };
-Node* Node::getNext() {
-	return next;
+template <typename T> void Node<T>::setPrevious(Node<T>* Previous) {
+    this>Previous = Previous;
 };
-Node* Node::getPrevious() {
-	return previous;
-}
 
 // -------------------- LINKED LIST --------------------
-class LinkedList {
-	public:
-		// Constructors
-		LinkedList(); // Empty constructor
-		LinkedList(auto);
-		// Functions
-		int searchByValue(auto); // search by value, return index
-		auto searchByIndex(int); // search by index, return value
-		int insert(auto, int index);
-		int append(auto); // Append new value
-		int remove(auto); // Remove first value found
-		int pop(); // Remove index 0
-		int pop(int); // Remove by index
-		int getLength();
-	private:
-		nodePtr head;
-		int length;
+template <typename T> class LinkedList {
+    public:
+        // Constructors
+        LinkedList(); // Empty Constructor
+        LinkedList(const LinkedList<T>&); // Copy Constructor
+        // Functions
+        int searchByValue(T);
+        T searchByIndex(int);
+        void insert(int, T);
+        void append(T);
+        T pop(int);
+        T pop();
+        T remove(T);
+        int getLength();
+        T getHead();
+    private:
+        Node<T>* Head;
+        int Length;
 };
-typedef LinkedList* LLptr;
-
-// ---------- Linked List Constructors ----------
-LinkedList::LinkedList() {
-	head = new Node();
-	length = 1;
+// ---------- LINKED LIST CONSTRUCTORS ----------
+template <typename T> LinkedList<T>::LinkedList() {
+    Head = new Node<T>();
+    Length = 1;
 };
-LinkedList::LinkedList(auto Value) {
-	head = new Node(Value);
-	length = 1;
+template <typename T> LinkedList<T>::LinkedList(const LinkedList<T>& copy) {
+    Head = copy->getHead();
+    Length = copy->getLength();
 };
-
-// ---------- Functions ----------
-int LinkedList::searchByValue(auto Value) {
-	try {
-		nodePtr temp = head;
-		int index = 0;
-		while (temp != NULL) {
-			if (temp->getValue() == Value) {
-				return index;
-			}
-			temp = temp->getNext();
-			index++;
-		}
-		return -1;
-	}
-	catch (...) {
-		return -1;
-	}
+// ---------- LINKED LIST FUNCTIONS ----------
+template <typename T> int LinkedList<T>::searchByValue(T Value) {
+    Node<T>* temp = Head;
+    int index = 0;
+    while (temp != NULL) {
+        if (temp.getValue()) {
+            return index;
+        }
+        temp = temp->getNext();
+        index++;
+    }
+    return -1;
 };
-auto LinkedList::searchByIndex(int Index) {
-	try {
-		if (Index > length) {
-			return -1;
-		}
-		nodePtr temp = head;
-		for (int i=0; i <= Index; i++) {
-			if (i == Index) {
-				return static_cast<int>(reinterpret_cast<intptr_t>(temp->getValue()));
-			}
-			temp = temp->getNext();
-		}
-		return -1; // Compiler message: "warning: control reaches end of non-void function [-Wreturn-type]"
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> T LinkedList<T>::searchByIndex(int Index) {
+    if (Index > Length) {
+        return ;
+    }
+    if (Index < 0) {
+        return ;
+    }
+    Node<T>* temp = Head;
+    for (int i=0; i <= Index; i++) {
+        if (i == Index) {
+            return temp->getValue();
+        }
+        temp = temp->getNext();
+    }
+    return ;
 };
-int LinkedList::insert(auto Value, int Index) {
-	try {
-		if (Index > length) {
-			return -1;
-		}
-		nodePtr newNode = new Node(Value);
-		nodePtr temp = head;
-		if (length == 0) {
-			head = newNode;
-			return 1;
-		}
-		for (int i=0; i < Index; i++) {
-			if (i == (Index-1)) {
-				nodePtr next = temp->getNext();
-				next->setPrevious(newNode);
-				newNode->setPrevious(temp);
-				newNode->setNext(next);
-				temp->setNext(newNode);
-				length++;
-				return 1;
-			}
-			temp = temp->getNext();
-		}
-		return -1; // Compiler message: "warning: control reaches end of non-void function [-Wreturn-type]"
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> void LinkedList<T>::insert(int Index, T Data) {
+    if (Index > Length) {
+        return ;
+    }
+    if (Index < 0) {
+        return ;
+    }
+    Node<T>* newNode = new Node<T>(Data);
+    Node<T>* temp = Head;
+    if (Length == 0) {
+        Head = newNode;
+        return ;
+    }
+    for (int i=0; i < Index; i++) {
+        if (i == (Index-1)) {
+            Node<T>* next = temp->getNext();
+            next->setPrevious(newNode);
+            newNode->setPrevious(temp);
+            newNode->setNext(next);
+            temp->setNext(newNode);
+            Length++;
+        }
+        temp = temp->getNext();
+    }
+    while (temp->getPrevious() != NULL) {
+        temp = temp->getPrevious();
+    }
+    Head = temp;
 };
-int LinkedList::append(auto Value) {
-	try {
-		nodePtr temp = head;
-		nodePtr newNode = new Node(Value);
-		if (length == 0) {
-			head = newNode;
-			return 1;
-		}
-		while (temp->getNext() != NULL) {
-			temp = temp->getNext();
-		}
-		temp->setNext(newNode);
-		while (temp->getPrevious() != NULL) {
-			temp = temp->getPrevious();
-		}
-		head = temp;
-		length++;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> void LinkedList<T>::append(T Data) {
+    this->insert(this->getLength(), Data);
 };
-int LinkedList::remove(auto Value) {
-	try {
-		int index = this->searchByValue(Value);
-		return this->pop(index);
-	}
-	catch (...) {
-		return -1;
-	}
-};
-int LinkedList::pop() {
-	try {
-		return this->pop(0);
-	}
-	catch (...) {
-		return -1;
-	}
-};
-int LinkedList::pop(int Index) {
-	try {
-		nodePtr temp = head;
-		for (int i=0; i <= Index; i++) {
-			if (i == Index) {
-				nodePtr previous = temp->getPrevious();
-				nodePtr next = temp->getNext();
+template <typename T> T LinkedList<T>::pop(int Index) {
+    if (Index > Length) {
+        return ;
+    }
+    if (Index < 0) {
+        return ;
+    }
+    Node<T>* temp = Head;
+    for (int i=0; i <= Index; i++) {
+        if (i == Index) {
+            if (i == Index) {
+				Node<T>* previous = temp->getPrevious();
+				Node<T>* next = temp->getNext();
 				previous->setNext(next);
 				next->setPrevious(previous);
-				length--;
-				temp = previous;
-				break;
+				Length--;
+				return temp->getValue();
 			}
 			temp = temp->getNext();
-		}
-		while (true) {
-			if (temp->getPrevious() == NULL) {
-				head = temp;
-				break;
-			}
-			temp = temp->getPrevious();
-		}
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
+        }
+    }
+    while (temp->getPrevious() != NULL) {
+        temp = temp->getPrevious();
+    }
+    Head = temp;
+    return ;
 };
-int LinkedList::getLength() {
-	return length;
-}
+template <typename T> T LinkedList<T>::pop(void) {
+    return this->pop(0);
+};
+template <typename T> T LinkedList<T>::remove(T Value) {
+    int index = this->searchByValue(Value);
+    return this->pop(index);
+};
+template <typename T> int LinkedList<T>::getLength(void) {
+    return Length;
+};
+template <typename T> T LinkedList<T>::getHead(void) {
+    return Head;
+};
 
 // -------------------- STACK --------------------
-class Stack {
-	public:
-		// Constructors
-		Stack(); // Empty constructor
-		Stack(auto);
-		// Functions
-		int Add(auto);
-		int Pop();
-		auto Current();
-	private:
-		LLptr linkedList;
+template <typename T> class Stack {
+    public:
+        // Constructors
+        Stack(); // Empty Constructor
+        Stack(const Stack<T>&); // Copy Constructor
+        // Functions
+        void add(T);
+        T pop();
+        T current();
+        LinkedList<T>* getLinkedList();
+    private:
+        LinkedList<T>* LL;
 };
-typedef Stack* stackPtr;
-
-// ---------- Stack Constructors ----------
-Stack::Stack() {
-	linkedList = new LinkedList();
+// ---------- STACK CONSTRUCTORS ----------
+template <typename T> Stack<T>::Stack() {
+    LL = new LinkedList<T>();
 };
-Stack::Stack(auto Value) {
-	linkedList = new LinkedList(Value);
+template <typename T> Stack<T>::Stack(const Stack<T>& copy) {
+    LL = copy->getLinkedList();
 };
-
-// ---------- Stack Functions ----------
-int Stack::Add(auto Value) {
-	try {
-		return linkedList->append(Value);
-	}
-	catch (...) {
-		return -1;
-	}
+// ---------- STACK FUNCTIONS ----------
+template <typename T> void Stack<T>::add(T Value) {
+    LL->append(Value);
 };
-int Stack::Pop() {
-	try {
-		int length = linkedList->getLength();
-		return linkedList->pop(length-1);
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> T Stack<T>::pop(void) {
+    return LL->pop(LL->getLength());
 };
-auto Stack::Current() {
-	try {
-		return linkedList->searchByIndex(0);
-	}
-	catch (...) {
-		return -1;
-	}
-}
+template <typename T> T Stack<T>::current(void) {
+    return LL->searchByIndex(LL->getLength());
+};
+template <typename T> LinkedList<T>* Stack<T>::getLinkedList(void) {
+    return LL;
+};
 
 // -------------------- QUEUE --------------------
-class Queue {
-	public:
-		// Constructors
-		Queue(); // Empty constructor
-		Queue(auto);
-		Queue(nodePtr);
-		Queue(Queue*); // Copy constructor
-		// Functions
-		int Add(auto);
-		int Pop();
-		auto FirstItem();
-	private:
-		LLptr linkedList;
+template <typename T> class Queue {
+    public:
+        // Constructors
+        Queue(); // Empty Constructor
+        Queue(const Queue<T>&); // Copy Constructor
+        // Functions
+        void add(T);
+        T pop();
+        T current();
+        LinkedList<T>* getLinkedList();
+    private:
+        LinkedList<T>* LL;
 };
-typedef Queue* queuePtr;
-
-// ---------- Queue Constructors ----------
-Queue::Queue() {
-	linkedList = new LinkedList();
+// ---------- QUEUE CONSTRUCTORS ----------
+template <typename T> Queue<T>::Queue(void) {
+    LL = new LinkedList<T>();
 };
-Queue::Queue(auto Value) {
-	linkedList = new LinkedList(Value);
+template <typename T> Queue<T>::Queue(const Queue<T>& copy) {
+    LL = copy->getLinkedList();
 };
-Queue::Queue(nodePtr node) {
-	linkedList = new LinkedList(node);
+// ---------- QUEUE FUNCTIONS ----------
+template <typename T> void Queue<T>::add(T Data) {
+    LL->append(Data);
 };
-Queue::Queue(Queue* queue) {
-	linkedList = queue ->linkedList;
+template <typename T> T Queue<T>::pop(void) {
+    return LL->pop();
 };
-
-// ---------- Queue Functions ----------
-int Queue::Add(auto Value) {
-	try {
-		return linkedList->append(Value);
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> T Queue<T>::current(void) {
+    return LL->searchByIndex(0);
 };
-int Queue::Pop() {
-	try {
-		return linkedList->pop();
-	}
-	catch (...) {
-		return -1;
-	}
+template <typename T> LinkedList<T>* Queue<T>::getLinkedList(void) {
+    return LL;
 };
-auto Queue::FirstItem() {
-	try {
-		int length = linkedList->getLength() - 1;
-		return linkedList->searchByIndex(length);
-	}
-	catch (...) {
-		return -1;
-	}
-}
-
-// -------------------- GRAPH (BREADTH FIRST SEARCH) --------------------
-class BreadthFirstGraph {
-	public:
-		// Constructors
-		BreadthFirstGraph(); // Empty Constructor
-		BreadthFirstGraph(BreadthFirstGraph*); // Copy Constructor
-		BreadthFirstGraph(auto);
-		BreadthFirstGraph(nodePtr);
-		// Functions
-		int AddToGraph(auto);
-		template <class> auto CurrentValue();
-		int RemoveLast();
-	private:
-		queuePtr queue;
-		int length;
-};
-typedef BreadthFirstGraph* BFSGraph;
-
-// ---------- Graph (Breadth First Search) Constructors ----------
-BreadthFirstGraph::BreadthFirstGraph() {
-	queue = new Queue();
-	length = 1;
-};
-
-BreadthFirstGraph::BreadthFirstGraph(BFSGraph graph) {
-	queue = graph->queue;
-	length = graph->length;
-};
-BreadthFirstGraph::BreadthFirstGraph(auto root) {
-	queue = new Queue(root);
-	length = 1;
-};
-BreadthFirstGraph::BreadthFirstGraph(nodePtr rootNode) {
-	queue = new Queue(rootNode);
-	length = 1;
-};
-
-// ---------- Graph (Breadth First Search) Functions ----------
-int BreadthFirstGraph::AddToGraph(auto newValue) {
-	try {
-		queue->Add(newValue);
-		length++;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
-};
-int BreadthFirstGraph::RemoveLast() {
-	try {
-		queue->Pop();
-		length--;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
-};
-template <class> auto BreadthFirstGraph::CurrentValue() {
-	try {
-		return queue->FirstItem();
-	}
-	catch (...) {
-		return -1;
-	}
-};
-
-// -------------------- GRAPH (DEPTH FIRST SEARCH) --------------------
-class DepthFirstGraph {
-	public:
-		// Constructors
-		DepthFirstGraph();
-		DepthFirstGraph(auto);
-		// Functions
-		int AddToGraph(auto);
-		template <class> auto CurrentValue();
-		int Remove();
-	private:
-		int length;
-		stackPtr stack;
-};
-typedef DepthFirstGraph* DFSgraph;
-
-// ---------- Graph (Depth First Search) Constructors ----------
-DepthFirstGraph::DepthFirstGraph() {
-	stack = new Stack();
-	length = 1;
-};
-DepthFirstGraph::DepthFirstGraph(auto graph) {
-	stack = new Stack(graph);
-	length = 1;
-};
-
-// ---------- Graph (Depth First Search) Functions ----------
-int DepthFirstGraph::AddToGraph(auto newValue) {
-	try {
-		stack->Add(newValue);
-		length++;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
-};
-int DepthFirstGraph::Remove() {
-	try {
-		stack->Pop();
-		length--;
-		return 1;
-	}
-	catch (...) {
-		return -1;
-	}
-};
-template <class> auto DepthFirstGraph::CurrentValue() {
-	try {
-		return stack->Current();
-	}
-	catch (...) {
-		return -1;
-	}
-};
-
 
 int main() {
-	return 1;
-}
+    return 1;
+};
